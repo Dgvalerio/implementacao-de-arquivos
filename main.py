64cf553c5683f96c52277a78d63834c22cbb18cd
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2023-09-23 16:42:10 -0300

    feat: change context from list to file

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-class Node:
-    def __init__(self, data=0, next_node=None):
-        self.data = data
-        self.next = next_node
+# Considere a implementação de arquivos com lista encadeada.
+# Um arquivo é dividido em vários blocos encadeados no disco.
+# Cada bloco deve possuir o tamanho de 1 byte (uma letra) e um ponteiro para o bloco seguinte,
+# sendo o último bloco com ponteiro nulo indicando seu final.
+class Block:
+    def __init__(self, letter=0, next_block=None):
+        self.letter = letter
+        self.next = next_block
 
     def __repr__(self):
-        return '%s -> %s' % (self.data, self.next)
+        return '%s -> %s' % (self.letter, self.next)
 
 
-class List:
+class File:
     def __init__(self):
         self.start = None
 
@@ -16,14 +20,14 @@ class List:
 
     # Para inserir no inicio da lista
     def unshift(self, new_data):
-        node = Node(new_data)
+        node = Block(new_data)
         node.next = self.start
         self.start = node
 
 
 if __name__ == '__main__':
-    list = List()
-    list.unshift(8)
-    print(list)
-    list.unshift(16)
-    print(list)
+    file = File()
+    file.unshift(8)
+    print(file)
+    file.unshift(16)
+    print(file)
